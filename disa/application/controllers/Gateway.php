315068bdf4b7685e1fe1fb49792992cbf438faf6
ghssvythiri <asif.kodur@gmail.com>
2018-01-18 16:05:53 +0530

    code formatted

--- disa/application/controllers/Gateway.php
+++ disa/application/controllers/Gateway.php
@@ -1,86 +1,69 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-
 class Gateway extends CI_Controller
-    {
-
-        function __construct()
-            {
-
-                parent::__construct();
-
-                $this->load->helper('url');
-                $this->load->library('session');
-                $this->load->library('mylibrary');
-                       
-                        
-            }
-        public function index()
-            {
-
-                $this->home();
-
-            }
-            public function home(){
-				redirect('register_student');
-			}
-            public function register_student()
-            {
-				$this->mylibrary->load_page("register_student");
-				
-				}
-            
-            public function login()
-            {
-				
-				
-					$this->mylibrary->load_page("login");
-					
-					}
-					
-				
-            
-           public  function dashboard()
-            { 
-				
-				$this->mylibrary->load_page("dashboard");
-		  }
-             function student_list()
-            { 
-				
-				$this->mylibrary->load_page("student_list");}
-            public function contact()
-            {}
-            public function about()
-            {}
-            public function login_submitted()
-            {	
-				if($this->mylibrary->login_check())// function validates user and set session data
-				{
-					redirect("dashboard");} //on success redirects to dashboard}
-					else{ $this->login(); // Redirects to login page on failure
-				
-				}}
-			public function registration_submitted()
-			{
-				
-				$this->mylibrary->registration_form_submitted();
-			}
-             public function logout(){
-				 $this->mylibrary->logout();
-				 redirect('login');}
-				 
-				public function delete_student($id = '', $email = '')
-				
-				{
-					
-					$this->mylibrary->delete_student($id, $email);
-					redirect('student_list');
-				}
-    }
-
+{
+                function __construct()
+                {
+                                parent::__construct();
+                                $this->load->helper('url');
+                                $this->load->library('session');
+                                $this->load->library('mylibrary');
+                }
+                public function index()
+                {
+                                $this->home();
+                }
+                public function home()
+                {
+                                redirect('register_student');
+                }
+                public function register_student()
+                {
+                                $this->mylibrary->load_page("register_student");
+                }
+                public function login()
+                {
+                                $this->mylibrary->load_page("login");
+                }
+                public function dashboard()
+                {
+                                $this->mylibrary->load_page("dashboard");
+                }
+                function student_list()
+                {
+                                $this->mylibrary->load_page("student_list");
+                }
+                public function contact()
+                {
+                }
+                public function about()
+                {
+                }
+                public function login_submitted()
+                {
+                                if ($this->mylibrary->login_check()) // function validates user and set session data
+                                                {
+                                                redirect("dashboard");
+                                } //on success redirects to dashboard}
+                                else {
+                                                $this->login(); // Redirects to login page on failure
+                                }
+                }
+                public function registration_submitted()
+                {
+                                $this->mylibrary->registration_form_submitted();
+                }
+                public function logout()
+                {
+                                $this->mylibrary->logout();
+                                redirect('login');
+                }
+                public function delete_student($id = '', $email = '')
+                {
+                                $this->mylibrary->delete_student($id, $email);
+                                redirect('student_list');
+                }
+}
 ?>
-
 
 
--- disa/application/views/student_list/student_list_view.php
+++ disa/application/views/student_list/student_list_view.php
@@ -1,9 +1,10 @@
-<?php
+
+	<?php
 /**
-* Class and Function List:
-* Function list:
-* Classes list:
-*/
+ * Class and Function List:
+ * Function list:
+ * Classes list:
+ */
 // Page creates a table and deploys elements of $data['student_list] parameter
 defined('BASEPATH') OR exit('No direct script access allowed');
 $all_dbcolumns = array(
@@ -28,61 +29,80 @@ $all_dbcolumns = array(
         'interest',
         'career_aspiration'
 );
-$no = 0;
+$no            = 0;
 ?>
 <table><tr><th>Sl No</th>
 
- <?php if ($user_type == 'root'): ?>
+ <?php
+if ($user_type == 'root'):
+?>
  
   <th>Edit</th><th>Delete</th>
   
   <?
-endif; ?>
-  
-  <?php foreach ($all_dbcolumns as $column): //Lists headers of table
-         ?>
+endif;
+?>
  
-      <?php if ($column != 'student_id'): ?>
-      <th><?=ucfirst($column) ?></th> <? // to skip student id
-                 ?>
-      <?
-        endif; ?><?php
-endforeach; ?>
+  <?php
+foreach ($all_dbcolumns as $column): //Lists headers of table
+?>
+ 
+      <?php
+        if ($column != 'student_id'):
+?>
+     <th><?= ucfirst($column) ?></th> <? // to skip student id
+?>
+     <?
+        endif;
+?><?php
+endforeach;
+?>
  
 
-<?php foreach ($student_list as $row): ?>
-        <?php $no = $no + 1; ?>
-        <tr><td><?=$no
-?></td>  
-           
+<?php
+foreach ($student_list as $row):
+?>
+       <?php
+        $no = $no + 1;
+?>
+       <tr><td><?= $no ?></td>  
+          
        <?
         /* Adds Edit and Delete if logged in as root */
 ?>
-        <?php if ($user_type == 'root'):
-
-                $std_id = $row['student_id'];
+       <?php
+        if ($user_type == 'root'):
+                $std_id    = $row['student_id'];
                 $std_email = urlencode($row['email']);
 ?>
-                
-        <td><a href='edit.php?id=<?=$std_id
-?>&email=<?=$std_email
-?>'>Edit</a></td>
-        <?php $warning = '"Do you really want to delete ' . $row["name"] . '?"'; ?>
-        <td><a href=<?php echo site_url('delete_student'); ?>/<?=$std_id ?>/<?=$std_email ?> onclick='return confirm(<?=$warning ?>)'>Delete</a></td>
+               
+        <td><a href='edit.php?id=<?= $std_id ?>&email=<?= $std_email ?>'>Edit</a></td>
+        <?php
+                $warning = '"Do you really want to delete ' . $row["name"] . '?"';
+?>
+       <td><a href=<?php
+                echo site_url('delete_student');
+?>/<?= $std_id ?>/<?= $std_email ?> onclick='return confirm(<?= $warning ?>)'>Delete</a></td>
     <?php
-        endif; ?>
-        
-        <?php foreach ($row as $key => $value): ?>
-        <? if ($key != 'student_id' and $key != 'created_time' and $key != 'modified_time' and $key != 'deleted'): #to Skip Student ID,created time etc
-                         ?>
-         <td><?=$value
-?></td>
+        endif;
+?>
+       
+        <?php
+        foreach ($row as $key => $value):
+?>
+       <?
+                if ($key != 'student_id' and $key != 'created_time' and $key != 'modified_time' and $key != 'deleted'): #to Skip Student ID,created time etc
+?>
+        <td><?= $value ?></td>
          <?php
-                endif; ?>                 
+                endif;
+?>                
     <?php
-        endforeach; ?>
-    </tr>
+        endforeach;
+?>
+   </tr>
   
 <?php
-endforeach; ?>
+endforeach;
+?>
 </table>
